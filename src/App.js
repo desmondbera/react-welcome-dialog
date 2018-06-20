b955d3e92bc7754bec6a192775f2571a7249b0e2
Desmond <desmond.bera@gmail.com>
2018-06-20 18:59:07 -0400

    more specific components

--- src/App.js
+++ src/App.js
@@ -7,17 +7,21 @@ function FancyBorder(props) {
     <div className={'FancyBorder FancyBorder-' + props.color}>
       {props.children}
     </div>
+  )
+}
+
+function Dialog(props) {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">{props.title}</h1>
+      <p className="Dialog-message">{props.message}</p>
+    </FancyBorder>
   );
 }
 
 function WelcomeDialog() {
   return (
-    <FancyBorder color="blue">
-      <h1 className="Dialog-title">Welcome</h1>
-      <p className="Dialog-message">
-        Thank you for visiting our spacecraft!
-      </p>
-    </FancyBorder>
+    <Dialog title="Welcome" message="Thank you for visiting our spacecraft!"/>
   );
 }
 
